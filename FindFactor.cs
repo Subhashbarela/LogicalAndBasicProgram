e496f925fe017b6b15bc7af38b7e10dd203ba1bf
Subhash Barela <barelasubhash16@gmail.com>
2023-02-17 00:00:36 -0800

    commit Factor Opf Number

---
+++ FindFactor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingSeries
+{
+    internal class FindFactor
+    {
+        public static void Factor()
+        {
+            Console.WriteLine("Enter the number to find factor : ");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int fact;
+            for(fact=1;fact<num;fact++)
+            {
+                if(num%fact == 0)
+                {
+                    Console.Write(fact + ",");
+                }
+            }
+        }
+    }
+}
